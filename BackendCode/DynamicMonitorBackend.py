a8f84c558be163a5f8063f266704b27f73e81a96
Maya Srikanth <mayassrikanth@gmail.com>
2021-06-24 14:59:12 -0400

    including updated time series script and backeend script

--- BackendCode/DynamicMonitorBackend.py
+++ BackendCode/DynamicMonitorBackend.py
@@ -3,26 +3,29 @@ This is an intermediate backend script for the data vis platform. It pulls
 new twitter data, preprocesses it, runs gloves, runs visualizations, and
 commits new datafiles to the repo which hosts the frontend for the platform.
 '''
-from git import Repo
+
+from github import Github
 import os
+
 import subprocess
 from preprocess_efficient import *
 from word_dist import *
 import argparse
 import pandas as pd
 import datetime
+from time_series_model import *
+
+
 
 ''' USER-DEFINED VARIABLES.
 - repo_dir: directory of user's remote Github repository containing all dynamic monitor data.
 - glove_path: path to "glove/" directory.
 - working_dir: path to directory that stores all .csv outputs from word_dist.py
-and time_series.py (tsne plots, arima forecast plots, raw counts plots)
+and time_series.py (tsne plots, arima forecast plots)
  '''
-
-data_repo_dir = 'github_username/DynamicMonitor/'
+data_repo_dir = 'github_username/DynamicMonitor'
 glove_path = 'glove/'
-
-working_dir = './dynamic-computations/data/'
+working_dir = 'local_data/'
 
 ''' SCRIPT-DEFINED VARIABLES.
 - monitor_time_step: datetime timestep indexing latest monitor files/visualizations,
@@ -37,31 +40,50 @@ keyword_list = []
 def init_pipeline(datafile, time_step, keywords):
     '''Preprocesses new data, runs glove, creates visuals, updates
     frontend of UI. '''
+    # ensure working directory which holds all intermediate .csv files exists
+    if not os.path.exists(working_dir):
+        os.makedirs(working_dir)
 
     # Pull updated keywords from remote repo
     update_keyword_list() # Update keywords list
     monitor_time_step = time_step # Update timestep for file indexing
 
     token = os.getenv('GITHUB_PAT', '...')
-    infile_name = preprocess_data(datafile)  # Preprocess Data
+    get_day_counts(datafile) # run arima on day counts
+    get_hour_counts(datafile) # run arima on hour counts
 
+    infile_name = preprocess_data(datafile)  # Preprocess Data
     run_glove(infile_name) # Run GloVe
     create_visuals()
+
 
 def get_day_counts(infile_name):
     '''Obtain daily counts of keywords. Output sent for arima modelling. '''
     df = pd.read_csv(infile_name)
     df['created_at'] = pd.to_datetime(df['created_at'])
     df = df.sort_values(by='created_at',ascending=True)
+    # set datetime index
+    df.index = df.set_index('created_at')
+    df.dropna() # drop nan rows
+
     for keyword in keyword_list:
         print('Extracting daily counts for' + keyword)
-        df_temp = df[df.text.str.contains(keyword)]
+        df_temp = df.loc[df.text.str.contains(keyword, na=False)]
         df_temp = df_temp.groupby([pd.Grouper(key='created_at',freq='D')]).size().reset_index(name='Count')
+        # fill in missing days with 0
+        df_temp.index = df_temp['created_at']
+        # Reset index & fill missing hours with counts=0
+        idx = pd.date_range(df_temp['created_at'].min(), df_temp['created_at'].max(), freq='D')
+        df_temp = df_temp.reindex(idx, fill_value=0)
         if '#' in keyword:
             fname = 'hash_' + keyword[1:] + '_' + str(monitor_time_step) + '_daycounts.csv'
         else:
             fname = keyword + '_' + str(monitor_time_step) + '_daycounts.csv'
-        df_temp.to_csv(glove_dir + '/data/' + fname)
+
+        # Call time series modelling
+        fname_res = working_dir + '/data/' + fname
+        df_temp.to_csv(fname_res)
+        time_series_model(fname_res, monitor_time_step, keyword, working_dir)
 
 
 def get_hourly_counts(infile_name):
@@ -69,15 +91,29 @@ def get_hourly_counts(infile_name):
     df = pd.read_csv(infile_name)
     df['created_at'] = pd.to_datetime(df['created_at'])
     df = df.sort_values(by='created_at',ascending=True)
+    # set datetime index
+    df.index = df.set_index('created_at')
+    df.dropna()
+
     for keyword in keyword_list:
         print('Extracting hourly counts for ' + keyword)
-        df_temp = df[df.text.str.contains(keyword)]
+        df_temp = df.loc[df.text.str.contains(keyword, na=False)]
         df_temp = df_temp.groupby([pd.Grouper(key='created_at',freq='H')]).size().reset_index(name='Count')
+        df_temp.index = df_temp['created_at']
+
+        # Reset index & fill missing hours with counts=0
+        idx = pd.date_range(df_temp['created_at'].min(), df_temp['created_at'].max(), freq='H')
+        df_temp = df_temp.reindex(idx, fill_value=0)
+
         if '#' in keyword:
             fname = 'hash_' + keyword[1:] + '_' + str(monitor_time_step) + '_hourcounts.csv'
         else:
             fname = keyword + '_' + str(monitor_time_step) + '_hourcounts.csv'
-        df_temp.to_csv(glove_dir + '/data/' + fname)
+
+        # Call time series modelling function with filename
+        fname_res = working_dir + '/data/' + fname
+        df_temp.to_csv(fname_res)
+        time_series_model(fname_res, monitor_time_step, keyword, working_dir)
 
 
 def preprocess_data(infile_name):
@@ -116,23 +152,44 @@ def create_visuals():
     '''Runs the visualization code from word_dist.py and saves the new info
     to .csv files. Updates remote repository data/ directory to show these files. '''
 
+    g = Github(token)
+    repo = g.get_repo(data_repo_dir)
+    file = repo.get_contents('data/LatestKeywords.csv', ref="main")
+
     # Check for dynamic-computations/data directory and create if not there.
     if not os.path.exists(working_dir):
         os.makedirs(working_dir)
 
-    get_tsne_visuals(keyword_list, 30, monitor_time_step)
-    # TODO: Add timestep to last row of LatestKeywords.csv & push changes.
+    get_tsne_visuals(keyword_list, 30, monitor_time_step, working_dir)
 
-    g = Github(token)
-    repo = g.get_repo(data_repo_dir)
+    # Update last row of LatestKeywords.csv with timestep & push changes.
+    git_raw_prefix = 'https://raw.githubusercontent.com/'
+    branch_name = 'main'
+    git_file_path = git_raw_prefix + data_repo_dir + '/' + branch_name + '/data/LatestKeywords.csv'
+    df = pd.read_csv(git_file_path, error_bad_lines=False)
+
+    monitor_time_step.replace(' ', '_')
+    temp = df.Keywords.values
+    # assuming timestep is last element
+    temp[len(temp)-1] = monitor_time_step
+    # Save back into df
+    df.Keywords = temp
+    df.to_csv(working_dir + 'LatestKeywords.csv')
+
+    keywords_path = working_dir + 'LatestKeywords.csv'
+    with open(keywords_path, 'r') as file:
+        content = file.read()
+
+    contents = repo.get_contents('data/LatestKeywords.csv', ref='main')  # grab contents of file
+    repo.update_file(contents.path, message, content, contents.sha, branch='main')
+
 
     for keyword in keyword_list: #f"{path}{keyword}_tsne{tsne_time_step}.csv"
         keyword_fm = keyword
         if '#' in keyword:
             keyword_fm = 'hash_' + keyword[1:]
-
-        filepath_tsne = "./dynamic-computations/data/" + keyword_fm + "_tsne" + monitor_time_step + ".csv"
-        if os.path.exists(fname_tsne):
+        filepath_tsne = "local-data/" + keyword_fm + "_tsne" + monitor_time_step + ".csv"
+        if os.path.exists(fname_tsne): # if visual was created
             git_file_path = 'data/' + keyword_fm + '_tsne' + monitor_time_step + '.csv'
             with open(filepath_tsne, 'r') as file:
                 content = file.read()
---
+++ BackendCode/time_series_model.py
@@ -0,0 +1,88 @@
+''' time_series_model.py should be in same directory as DynamicMonitorBackend.py
+
+A fun link for some theory behind arima: https://otexts.com/fpp2/stationarity.html
+The general breakdown of tasks this script does:
+1. Parse twitter time series data, split into test / train
+2. Log Transform
+3. Choose best fit with python auto_arima
+4. Use model to project 10 timesteps into future.
+5. Output results to .csv file, index with correct timestep
+6. Push changed files to github remote repo.
+REPEAT for each keyword.
+
+'''
+
+import pandas as pd
+from github import Github
+import os
+
+from pmdarima.pipeline import Pipeline
+from pmdarima.preprocessing import LogEndogTransformer
+
+import pmdarima as pm
+from pmdarima.arima import StepwiseContext, AutoARIMA
+
+data_repo_dir = 'mayasrikanth/mayasrikanth.github.io/'
+
+
+def time_series_model(fname, monitor_time_step, keyword, output_dir):
+    df = pd.read_csv(fname)
+    data = df.Count.values
+
+    # split into train and test (actually not necessary)
+    test_idx = int(len(data) * 0.75)
+    train, test = data[:test_idx], data[test_idx:]
+
+    # create preprocessing & training pipeline
+    pipeline = Pipeline([
+    ("log", LogEndogTransformer()),
+    ("arima", AutoARIMA(seasonal=False, stepwise=True,
+                        start_p=1, start_q=1,
+                        #max_p=5, max_q=5
+                        suppress_warnings=True,
+                        error_action='ignore'))
+                        ])
+
+    # fit stepwise auto-ARIMA
+    pipeline.fit(data)
+
+    # forecast 10 steps into future & obtain projection conf intervals
+    preds, conf_int = pipeline.predict(n_periods=10, return_conf_int=True)
+    print("\nForecasts:")
+    print(preds)
+
+    # generate .csv file with conf_low, conf_area, and series
+    padding = np.zeros(len(data))
+    conf_low = np.concatenate((data, conf_int[:,0]))
+    conf_high = np.concatenate((padding, conf_int[:,1]))
+    conf_area = np.concatenate((padding, conf_int[:,0] + conf_int[:,1]))
+    freq = np.concatenate((data, preds))
+
+    # Note that inverse of log is automatically applied.
+    freq = np.concatenate(data, preds)
+
+    df_output = pd.DataFrame({'freq': freq, 'conf_low':conf_low, \
+                              'conf_area': conf_area})
+
+    output_fpath = output_dir + keyword + "_" + str(monitor_time_step) + "_forecast.csv"
+    df_output.to_csv(output_fpath)
+
+    # call update_remote_repo to push this file to your remote data directory
+    update_remote_repo(output_fpath, keyword, monitor_time_step)
+
+
+
+def update_remote_repo(output_fpath, keyword, monitor_time_step):
+        token = os.getenv('GITHUB_PAT', '...')
+        g = Github(token)
+        repo = g.get_repo(data_repo_dir)
+
+        # Pushing file to github
+        with open(output_fpath, 'r') as file: # reading new .csv containing forecasts
+            content = file.read()
+
+        monitor_time_step.replace(' ', '_')
+        git_file_path = 'data/' + keyword + '_' + monitor_time_step + '_forecast.csv'
+        repo.create_file(git_file_path, "committing updated time series", \
+                        content, branch="main")
+        print(git_file_path + ' CREATED')
